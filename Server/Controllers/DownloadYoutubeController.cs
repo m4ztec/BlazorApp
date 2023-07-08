1fadd4416d9a0e4e46f0d56665a697cac32c08e2
m4ztec <manueldias97@hotmail.com>
2023-07-08 15:59:51 +0100

    fully funtional youtube download

--- Server/Controllers/DownloadYoutubeController.cs
+++ Server/Controllers/DownloadYoutubeController.cs
@@ -8,15 +8,15 @@ namespace BlazorApp.Server.Controllers;
 [ApiController]
 public class DownloadYoutubeController : ControllerBase
 {
-    [HttpGet("{videoId}")]
-    public async Task<IActionResult> GetVideoAudio(string videoId)
+    [HttpGet("GetAudioFile/{videoId}")]
+    public async Task<IActionResult> GetAudioFile(string videoId)
     {
         //var videoId = "dQw4w9WgXcQ";
         var youtube = new YoutubeClient();
         var streamManifest = await youtube.Videos.Streams.GetManifestAsync(videoId);
         var streamInfo = streamManifest.GetAudioOnlyStreams().GetWithHighestBitrate();
-        var videoTitle = await youtube.Videos.GetAsync(videoId);
-        var videoFileName = $"{videoTitle.Title}.{streamInfo.Container}";
+        var video = await youtube.Videos.GetAsync(videoId);
+        var videoFileName = $"{video.Title}.{streamInfo.Container}";
         var filePath = Path.Combine(Directory.GetCurrentDirectory(), "Data", videoFileName);
         await youtube.Videos.Streams.DownloadAsync(streamInfo, filePath);
 
@@ -27,5 +27,13 @@ public class DownloadYoutubeController : ControllerBase
         }
         memory.Position = 0;
         return File(memory, "video/mp4", Path.GetFileName(filePath));
+    }
+
+    [HttpGet("GetVideoName/{videoId}")]
+    public async Task<IActionResult> GetVideoName(string videoId)
+    {
+        var youtube = new YoutubeClient();
+        var video = await youtube.Videos.GetAsync(videoId);
+        return Ok($"{video.Title}.mp4");
     }
 }

7feff7910ce6b4ac530cea348150d5afb12c3f21
m4ztec <manueldias97@hotmail.com>
2023-07-23 17:18:37 +0100

    Add connection to postgresSQL db

---
+++ Server/Controllers/TodoController.cs
@@ -0,0 +1,34 @@
+﻿using BlazorApp.Shared;
+using Dapper;
+using Microsoft.AspNetCore.Mvc;
+using Npgsql;
+
+namespace BlazorApp.Server.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class TodoController : ControllerBase
+{
+    private readonly IConfiguration _configuration;
+    private readonly NpgsqlConnection _connection;
+
+    public TodoController(IConfiguration configuration)
+    {
+        _configuration = configuration;
+        var _ConnectionStrings = _configuration.GetConnectionString("DefaultConnection");
+        _connection = new NpgsqlConnection(_ConnectionStrings);
+    }
+
+    [HttpGet("all")]
+    public async Task<Todo[]> GetRows()
+    {
+        await _connection.OpenAsync();
+
+        var hi = await _connection.QueryAsync<Todo>("SELECT Id, Title, isCompleted FROM todo");
+
+        await _connection.CloseAsync();
+
+        return hi.ToArray();
+    }
+
+}
---
+++ Shared/Todo.cs
@@ -0,0 +1,8 @@
+﻿namespace BlazorApp.Shared;
+
+public class Todo
+{
+    public int Id { get; set; }
+    public string? Title { get; set; }
+    public bool IsCompleted { get; set; }
+}

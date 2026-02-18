ca1032a5319b9ee0679e373a92a0bea23a91fca2
m4ztec <manuel97dias@gmail.com>
2026-02-18 18:26:21 +0000

    trying new stuff

--- YoutubeDownloadScripter/Program.cs
+++ YoutubeDownloadScripter/Program.cs
@@ -1,5 +1,5 @@
 ﻿using YoutubeDownloadScripter;
 
-var url = "https://www.youtube.com/watch?v=3Zutr6Yhzk0";
+var url = "https://invidious.nerdvpn.de/watch?v=3Zutr6Yhzk0";
 
 await Video.GetVideo(url);
--- YoutubeDownloadScripter/Video.cs
+++ YoutubeDownloadScripter/Video.cs
@@ -1,3 +1,6 @@
+using VideoLibrary;
+using YoutubeDLSharp;
+using YoutubeDLSharp.Options;
 using YoutubeExplode;
 using YoutubeExplode.Common;
 using YoutubeExplode.Videos.Streams;
@@ -23,20 +26,50 @@ public class Video
             await youtube.Videos.Streams.DownloadAsync(streamInfo, filePath);
         }
     }
-    
-    public static async Task GetVideo(string videoUrl, string? outputPath = null)
+
+    public static async Task<RunResult<string>> GetVideo(string videoUrl, string? outputPath = null)
     {
-         var youtube = new YoutubeClient();
+        string ytDlpPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "yt-dlp.exe");
 
-        var video = await youtube.Videos.GetAsync(videoUrl);
+        if (!File.Exists(ytDlpPath))
+        {
+            Console.WriteLine("Downloading yt-dlp...");
+            await DownloadYtDlp(ytDlpPath);
+        }
 
-        var streamManifest = await youtube.Videos.Streams.GetManifestAsync(video.Id.Value);
-        var streamInfo = streamManifest.GetVideoStreams().Where(a => a.Container == Container.Mp4).GetWithHighestVideoQuality();
+        string downloadsPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.UserProfile), "Downloads");
+Console.WriteLine($"Videos will be saved to: {downloadsPath}");
+        var yt = new YoutubeDL
+        {
+            YoutubeDLPath = ytDlpPath,
+            OutputFolder = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.UserProfile), "Downloads"),
+            OutputFileTemplate = "%(title)s.%(ext)s"
+        };
 
-        var title = video.Title.WithoutInvalidChars();
-        var videoFileName = $"{title}.{streamInfo.Container}";
-        var filePath = outputPath ?? Path.Combine(Directory.GetCurrentDirectory(), "Data", videoFileName);
+        var options = new OptionSet
+        {
+            Format = "bestaudio/best",
+            ExtractAudio = true,
+            AudioFormat = AudioConversionFormat.Best,
+            //Proxy = "https://inv.riverside.rocks",
+        AgeLimit = 99,
 
-        await youtube.Videos.Streams.DownloadAsync(streamInfo, filePath);
+            //AudioMultistreams  = true
+        };
+        var hi = await yt.RunVideoDownload(videoUrl, overrideOptions: options);
+        var hi2 =  hi.Success;
+
+        return hi; 
+    }
+    
+    static async Task DownloadYtDlp(string savePath)
+    {
+        string ytDlpUrl = "https://github.com/yt-dlp/yt-dlp/releases/latest/download/yt-dlp.exe";
+
+        using var client = new HttpClient();
+        var response = await client.GetAsync(ytDlpUrl);
+        response.EnsureSuccessStatusCode();
+        var content = await response.Content.ReadAsByteArrayAsync();
+        await File.WriteAllBytesAsync(savePath, content);
     }
 }
